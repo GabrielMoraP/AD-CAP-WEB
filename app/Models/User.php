a0585cb173e1de3e16f39f824e8c38c497a3d0d9
Gabriel <gabrielmorap03@gmail.com>
2025-03-17 09:20:01 +0000

    production-config

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\HasAvatar;
 
-class User extends Authenticatable implements HasAvatar
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -54,5 +56,12 @@ class User extends Authenticatable implements HasAvatar
     {
         $avatarId = $this->avatar ?? 1;
         return asset("storage/avatars/{$avatarId}.png");
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'Administrador' 
+            || $this->role === 'Editor'
+            || $this->role === 'Consultor';
     }
 }
